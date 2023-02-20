3ed18a4220fa2f1cfab422bcd14c2d29acacaa8f
Ahmyaaa <122892317+Ahmyaaa@users.noreply.github.com>
2023-02-20 13:13:23 +0800

    2 files modified

--- Midterms/mid-serve-static/index.js
+++ Midterms/mid-serve-static/index.js
@@ -1,23 +1,33 @@
-//1. Installing Express
-//2. Installing libraries/ dependencies
-//3. Require statements\\
-
 const express = require('express');
 const app = express();
 
-//4. use the middleware required for serving static files
-
 app.use(express.static('public'));
 
-//create the route to serve a static index.html
-
-app.get('/', (req, res) => {
+app.get('/index', (req, res) => {
   res.sendFile(__dirname + '/' + 'index.html');
 });
 
-//Setting the listener to ENV PORT info
-const port = process.env.PORT || 3000;
+app.get('/blog', (req, res) => {
+  res.sendFile(__dirname + '/' + 'blog.html');
+});
 
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/' + 'about.html');
+});
+
+app.get('/upload', (req, res) => {
+  res.sendFile(__dirname + '/' + 'upload.html');
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/' + 'contact.html');
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Error 404 page not found.</h1>');
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
